20fe422a45d74300d79a24e5a0e3d48968e677f8
HensleyAi <jakealanhensley@gmail.com>
2025-11-26 23:59:41 -0600

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./hho.db")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-APP_ENV = os.getenv("APP_ENV", "local")
